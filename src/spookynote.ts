f76b4027e297484c2fb776bf5f75816a936fdfbe
ScuffedItalian <anthony.locascio@outlook.com>
2022-11-17 19:38:58 -0800

    Added .push cuz yes

--- src/spookynote.ts
+++ src/spookynote.ts
@@ -1,10 +1,11 @@
-import {Vec3, Note} from "https://deno.land/x/remapper@3.0.0/src/mod.ts"; // MAKE SURE TO USE THE LATEST REMAPPER VERSION HERE
+import {Note} from "https://deno.land/x/remapper@3.0.0/src/mod.ts"; // MAKE SURE TO USE THE LATEST REMAPPER VERSION HERE
 
-export function SpookyNote(tStart: number, tEnd: number, NumberOfNote: number, colorOfNote: Vec3){
+export function SpookyNote(tStart: number, tEnd: number, NumberOfNote: number){
     for(let i = 0; i < NumberOfNote; i ++){
         const ScaryNote = new Note(tStart, tEnd)
         ScaryNote.animate.definitePosition = [10, 10, 10]
         ScaryNote.color = [0.26666666666666666,0.25882352941176473,0.27450980392156865]
+        ScaryNote.push(true)
     }
 }
 
