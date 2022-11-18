21964e3e3809964d0e8dd3493981e4b084b8eca2
ScuffedItalian <anthony.locascio@outlook.com>
2022-11-18 00:39:48 -0800

    Added Author

--- src/ezeasings.ts
+++ src/ezeasings.ts
@@ -1,6 +1,9 @@
 
 import {} from "https://deno.land/x/remapper@3.0.0/src/mod.ts"; // MAKE SURE TO USE THE LATEST REMAPPER VERSION HERE
-
+/**
+ * 
+ * @author Swifter
+ */
 export function InQuad(elapsed: number, initialValue: number, amountOfChange: number, duration: number): number {
 	return amountOfChange * (elapsed / duration) * elapsed + initialValue;
 }
