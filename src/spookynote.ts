733fbb4f91006eea600e5544956305475a933d0e
ScuffedItalian <anthony.locascio@outlook.com>
2022-11-17 20:00:12 -0800

    type added

--- src/spookynote.ts
+++ src/spookynote.ts
@@ -3,6 +3,7 @@ import {Note} from "https://deno.land/x/remapper@3.0.0/src/mod.ts"; // MAKE SURE
 export function SpookyNote(tStart: number, tEnd: number, NumberOfNote: number){
     for(let i = 0; i < NumberOfNote; i ++){
         const ScaryNote = new Note(tStart, tEnd)
+        ScaryNote.type = 1
         ScaryNote.animate.definitePosition = [10, 10, 10]
         ScaryNote.color = [0.26666666666666666,0.25882352941176473,0.27450980392156865]
         ScaryNote.push(true)
